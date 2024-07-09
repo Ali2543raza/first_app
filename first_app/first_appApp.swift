2f2e2741aa5f6a0915b54720347653f9afee78e5
Ali2543raza <raza08466@gmail.com>
2024-07-09 10:55:34 +0530

    Initial Commit

---
+++ first_app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first_app
+//
+//  Created by ALI RAZA on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
